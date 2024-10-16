657551a86ccff3ccc8c77bc24224f8d3ceff1800
Yahyap <yahyap20@gmail.com>
2024-10-17 00:06:38 +0700

    final sebelum semhas

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,50 +1,50 @@
-// document.addEventListener("DOMContentLoaded", async function () {
-//   try {
-//     const response = await fetch("http://localhost:8081/auth/protected", {
-//       method: "GET",
-//       credentials: "include",
-//     });
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    const response = await fetch("http://localhost:8081/auth/protected", {
+      method: "GET",
+      credentials: "include",
+    });
 
-//     const result = await response.json();
-//     if (response.ok) {
-//       let spanElement = document.getElementById("username");
-//       spanElement.textContent =
-//         result.user.firstname + ` ` + result.user.lastname;
-//     } else {
-//       window.location.href = "login.html";
-//     }
-//   } catch (error) {
-//     console.error("Error fetching home:", error);
-//     window.location.href = "login.html";
-//   }
-// });
+    const result = await response.json();
+    if (response.ok) {
+      let spanElement = document.getElementById("username");
+      spanElement.textContent =
+        result.user.firstname + ` ` + result.user.lastname;
+    } else {
+      window.location.href = "login.html";
+    }
+  } catch (error) {
+    console.error("Error fetching home:", error);
+    window.location.href = "login.html";
+  }
+});
 
-// const logoutButton = document.getElementById("logoutBtn");
-// if (logoutButton) {
-//   logoutButton.addEventListener("click", async function () {
-//     try {
-//       const response = await fetch("http://localhost:8081/auth/logout", {
-//         method: "POST",
-//         credentials: "include",
-//       });
+const logoutButton = document.getElementById("logoutBtn");
+if (logoutButton) {
+  logoutButton.addEventListener("click", async function () {
+    try {
+      const response = await fetch("http://localhost:8081/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
 
-//       const result = await response.json();
-//       if (response.ok) {
-//         window.location.href = "login.html";
-//       } else {
-//         alert(result.message);
-//       }
-//     } catch (error) {
-//       console.error("Error logging out:", error);
-//       alert("Logout failed. Please try again.");
-//     }
-//   });
-// }
+      const result = await response.json();
+      if (response.ok) {
+        window.location.href = "login.html";
+      } else {
+        alert(result.message);
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+      alert("Logout failed. Please try again.");
+    }
+  });
+}
 
-// function bacaartikel(id_artikel) {
-//   const url = `artikel?id_artikel=${id_artikel}`;
-//   window.location.href = url;
-// }
+function bacaartikel(id_artikel) {
+  const url = `artikel?id_artikel=${id_artikel}`;
+  window.location.href = url;
+}
 
 function bacaartikel1() {
   window.location.href =
