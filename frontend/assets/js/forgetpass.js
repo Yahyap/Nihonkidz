bf716e68ca4ebb80f56a10834e1f3e5da92467b5
Yahyap <yahyap20@gmail.com>
2024-12-13 20:14:57 +0700

    test

---
+++ frontend/assets/js/forgetpass.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const user_email_address = document.getElementById("InputEmail").value;
+    const user_password = document.getElementById("InputPassword").value;
+
+    const response = await fetch(
+      "https://auth-dot-sonic-totem-438312-d0.et.r.appspot.com/auth/forgot-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ user_email_address }),
+      }
+    );
+
+    const result = await response.json();
+    if (response.ok) {
+      let popContext = document.getElementById("quizEndModalLabel");
+      popContext.innerText = result.status;
+      let spanElement = document.getElementById("reason");
+      spanElement.textContent = result.message;
+      $("#quizEndModal").modal("show");
+    } else {
+      let popContext = document.getElementById("quizEndModalLabel");
+      popContext.innerText = result.status;
+      let spanElement = document.getElementById("reason");
+      spanElement.textContent = result.message;
+      $("#quizEndModal").modal("show");
+    }
+  });
